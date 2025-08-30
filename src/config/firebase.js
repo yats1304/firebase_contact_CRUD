c83a6da1a2e1f7cd4f13f0d9b36e71bbaba23ef8
yats1304 <chaubaly@gmail.com>
2025-08-30 18:55:20 +0530

    firebase connected

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyABVtWDq69xMVykJ_pWr_QAuP4qvcqp7OI",
-  authDomain: "vite-contact-f1dc2.firebaseapp.com",
-  projectId: "vite-contact-f1dc2",
-  storageBucket: "vite-contact-f1dc2.firebasestorage.app",
-  messagingSenderId: "948457943771",
-  appId: "1:948457943771:web:975e9ad4310dbf877c12fe",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
